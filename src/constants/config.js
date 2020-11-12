3ce0cb7b433826e30f05c66fd61f890f61edc7a0
blogs <yhz610323@gmail.com>
2020-11-12 23:28:57 +0800

    1

---
+++ src/constants/config.js
@@ -0,0 +1,12 @@
+export const jsonHeaders = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json'
+};
+
+export const site = {
+  siteMetadata: {
+    title: '今日热榜',
+    description:
+      '各站热点聚合：知乎热榜、V2EX热榜、微博热搜、贴吧热榜、豆瓣热榜、天涯热榜、虎扑热榜、Github热榜、华尔街见闻热榜'
+  }
+};
